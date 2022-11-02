e44b26e23ed988b2cc91f3e560b8cfd3f0cfe408
LeandroMonte <leandrobmonte@gmail.com>
2022-11-02 18:11:07 -0300

    Autenticação JWT validando token aula 06-09

--- spring-boot-forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoviaTokenFilter.java
+++ spring-boot-forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoviaTokenFilter.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
+import br.com.alura.forum.modelo.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,13 +12,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 public class AutenticacaoviaTokenFilter extends OncePerRequestFilter {
 
     TokenService tokenService;
+    private UsuarioRepository usuarioRepository;
 
-    public AutenticacaoviaTokenFilter(TokenService tokenService) {
+    public AutenticacaoviaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
         this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
     }
 
     @Override
@@ -24,7 +31,20 @@ public class AutenticacaoviaTokenFilter extends OncePerRequestFilter {
         String token = recuperarToken(httpServletRequest);
         Boolean valido = tokenService.isTokenValido(token);
         System.out.println(valido);
+
+        if(valido){
+            autenticaCliente(token);
+        }
+
         filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private void autenticaCliente(String token) {
+        Long idUsuario = tokenService.getIdUsuario(token);
+        Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
+
+        UsernamePasswordAuthenticationToken authetication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.get().getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authetication);
     }
 
     private String recuperarToken(HttpServletRequest httpServletRequest) {
