962a60f2a88b4c41f7abee31f6a4177e5ef6d271
LeandroMonte <leandrobmonte@gmail.com>
2022-11-02 15:00:40 -0300

    Paginação simplificada aula 07-10

--- spring-boot-forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,12 +43,8 @@ public class TopicosController {
 
     @GetMapping
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina,
-                                 @RequestParam int qtd,
-                                 @RequestParam String ordenacao) {
+                                 @PageableDefault(sort="id",direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.ASC, ordenacao);
-        
         if(nomeCurso == null){
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
