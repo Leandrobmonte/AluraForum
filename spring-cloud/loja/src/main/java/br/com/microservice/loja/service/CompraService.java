135103744e187331a8b76c49a60cbac89cc10529
LeandroMonte <leandrobmonte@gmail.com>
2022-11-06 18:18:08 -0300

    Registry com Eureka - Discovery Eureka aula 06-09

--- spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.microservice.loja.service;
 
 import br.com.microservice.loja.controller.dto.CompraDto;
 import br.com.microservice.loja.controller.dto.InfoFornecedorDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate client;
 
     public void realizaCompra(CompraDto compraDto) {
-        RestTemplate client = new RestTemplate();
         ResponseEntity<InfoFornecedorDto> exchange =
-                client.exchange("http://localhost:8081/info/"+ compraDto.getEndereco().getEstado()
+                client.exchange("http://fornecedor/info/"+ compraDto.getEndereco().getEstado()
              , HttpMethod.GET, null, InfoFornecedorDto.class);
 
         System.out.println(exchange.getBody().getEndereco());
