dae7a01a613b1e01acac07b8b4058e190f37cab7
LeandroMonte <leandrobmonte@gmail.com>
2022-11-06 16:55:04 -0300

    Quebrando Dominio em serviços aula 01-08

--- spring-cloud/loja/src/main/java/br/com/microservice/loja/controller/CompraController.java
+++ spring-cloud/loja/src/main/java/br/com/microservice/loja/controller/CompraController.java
@@ -1,4 +1,17 @@
 package br.com.microservice.loja.controller;
 
+import br.com.microservice.loja.controller.dto.CompraDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compra")
 public class CompraController {
+    @PostMapping
+    public void realizaCompra(@RequestBody CompraDto compraDto){
+
+    }
 }
