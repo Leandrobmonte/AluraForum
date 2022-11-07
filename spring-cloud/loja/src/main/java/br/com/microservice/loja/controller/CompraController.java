8c32a19ac1c5d11698f8a28c0fd0ad3b607e70bf
LeandroMonte <leandrobmonte@gmail.com>
2022-11-07 19:55:57 -0300

    Load Balance - Client Side aula 01-03

--- spring-cloud/loja/src/main/java/br/com/microservice/loja/controller/CompraController.java
+++ spring-cloud/loja/src/main/java/br/com/microservice/loja/controller/CompraController.java
@@ -3,6 +3,7 @@ package br.com.microservice.loja.controller;
 import br.com.microservice.loja.controller.dto.CompraDto;
 import br.com.microservice.loja.service.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class CompraController {
 
     @Autowired
     private CompraService compraService;
+
     @PostMapping
     public void realizaCompra(@RequestBody CompraDto compraDto){
         compraService.realizaCompra(compraDto);
--- spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.microservice.loja.service;
 import br.com.microservice.loja.controller.dto.CompraDto;
 import br.com.microservice.loja.controller.dto.InfoFornecedorDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,19 @@ import org.springframework.web.client.RestTemplate;
 public class CompraService {
 
     @Autowired
+    private DiscoveryClient eurekaClient;
+    @Autowired
     private RestTemplate client;
 
     public void realizaCompra(CompraDto compraDto) {
         ResponseEntity<InfoFornecedorDto> exchange =
                 client.exchange("http://fornecedor/info/"+ compraDto.getEndereco().getEstado()
              , HttpMethod.GET, null, InfoFornecedorDto.class);
+
+        eurekaClient.getInstances("fornecedor").stream()
+                        .forEach(fornecedor -> {
+                            System.out.println("localhost:" +fornecedor.getPort());
+                        });
 
         System.out.println(exchange.getBody().getEndereco());
 
