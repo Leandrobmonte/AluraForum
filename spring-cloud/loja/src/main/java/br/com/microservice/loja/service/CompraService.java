3fc615d60e5e8e3f302b73828b5aaf9ce27c8af5
LeandroMonte <leandrobmonte@gmail.com>
2022-11-07 20:08:23 -0300

    Load Balance - Spring Feing aula 04-05

--- spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ spring-cloud/loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -1,33 +1,46 @@
 package br.com.microservice.loja.service;
 
+import br.com.microservice.loja.client.FornecedorClient;
 import br.com.microservice.loja.controller.dto.CompraDto;
 import br.com.microservice.loja.controller.dto.InfoFornecedorDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
 
     @Autowired
-    private DiscoveryClient eurekaClient;
-    @Autowired
-    private RestTemplate client;
+    private FornecedorClient fornecedorClient;
 
     public void realizaCompra(CompraDto compraDto) {
-        ResponseEntity<InfoFornecedorDto> exchange =
-                client.exchange("http://fornecedor/info/"+ compraDto.getEndereco().getEstado()
-             , HttpMethod.GET, null, InfoFornecedorDto.class);
 
-        eurekaClient.getInstances("fornecedor").stream()
-                        .forEach(fornecedor -> {
-                            System.out.println("localhost:" +fornecedor.getPort());
-                        });
-
-        System.out.println(exchange.getBody().getEndereco());
+        InfoFornecedorDto info = fornecedorClient.getInfoPorEstado(compraDto.getEndereco().getEstado());
+        System.out.println(info.getEndereco());
 
     }
+
+
+
+    // Exemplo de como funciona com Load Balance do Ribbo com RestTemplate
+
+//    @Autowired
+//    private RestTemplate client;
+
+//    @Autowired
+//    private DiscoveryClient eurekaClient;
+
+//    public void realizaCompra(CompraDto compraDto) {
+//        ResponseEntity<InfoFornecedorDto> exchange =
+//                client.exchange("http://fornecedor/info/"+ compraDto.getEndereco().getEstado()
+//             , HttpMethod.GET, null, InfoFornecedorDto.class);
+//
+//        eurekaClient.getInstances("fornecedor").stream()
+//                        .forEach(fornecedor -> {
+//                            System.out.println("localhost:" +fornecedor.getPort());
+//                        });
+//
+//        System.out.println(exchange.getBody().getEndereco());
+//
+//    }
+
 }
